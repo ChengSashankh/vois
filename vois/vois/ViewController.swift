a93ad781e843f7242c351abc748e950c387bf660
Jiang Yuxin <jyx11011@gmail.com>
2020-03-14 15:43:04 +0800

    init

---
+++ vois/vois/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vois
+//
+//  Created by Jiang Yuxin on 14/3/20.
+//  Copyright © 2020 vois. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
