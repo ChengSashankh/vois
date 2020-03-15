1d3e64fa49cf4a799a720b0df7a7ed6defb45e02
Sashankh Chengavalli Kumar <cksashankh@gmail.com>
2020-03-15 18:16:15 +0800

    Implement simple recorder and playback

---
+++ vois/Vois/RecordingController.swift
@@ -0,0 +1,100 @@
+//
+//  RecordingController.swift
+//  Vois
+//
+//  Created by Sashankh Chengavalli Kumar on 15.03.20.
+//  Copyright © 2020 Vois. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class RecordingController {
+    var recordingSession: AVAudioSession
+    var audioRecorder: AVAudioRecorder
+    var recordingInProgress: Bool
+    var recordingCounter: Int
+
+    init(recordingCounter: Int) {
+        self.recordingSession = AVAudioSession()
+        self.audioRecorder = AVAudioRecorder()
+        self.recordingInProgress = false
+        self.recordingCounter = recordingCounter
+    }
+
+    init() {
+        self.recordingSession = AVAudioSession()
+        self.audioRecorder = AVAudioRecorder()
+        self.recordingInProgress = false
+        self.recordingCounter = 0
+    }
+
+    func getDefaultFileName() -> String {
+        recordingCounter += 1
+        return String("Recording_\(recordingCounter).m4a")
+    }
+
+    func getRecordingDirectoryPath() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectoryPath = paths[0]
+        return documentDirectoryPath
+    }
+
+    func startRecording(recorderDelegate: AVAudioRecorderDelegate) {
+        if recordingInProgress {
+            return
+        }
+
+        let recordingFileName = getDefaultFileName()
+        let recordingFilePath = getRecordingDirectoryPath().appendingPathComponent(recordingFileName)
+
+        let settingsDict = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+
+        do {
+            audioRecorder = try AVAudioRecorder(
+                url: recordingFilePath,
+                settings: settingsDict
+            )
+
+            audioRecorder.record()
+            audioRecorder.delegate = recorderDelegate
+            recordingInProgress = true
+        } catch {
+            // TODO: Handle error correctly
+            print("Something went wrong here!")
+        }
+    }
+
+    func stopRecording() {
+        if !recordingInProgress {
+            return
+        }
+
+        UserDefaults.standard.set(recordingCounter, forKey: "recordingCounter")
+        audioRecorder.stop()
+        recordingInProgress = false
+    }
+
+    func getCurrentRecordingDuration() -> TimeInterval {
+        return audioRecorder.currentTime
+    }
+
+    func getRecordings() -> [URL] {
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        do {
+            let fileURLs = try
+                FileManager.default.contentsOfDirectory(at: documentsURL, includingPropertiesForKeys: nil)
+            return fileURLs
+        } catch {
+            print("Error while enumerating files \(documentsURL.path): \(error.localizedDescription)")
+            return [URL]()
+        }
+    }
+
+    
+}
---
+++ vois/Vois/RecordingViewController.swift
@@ -0,0 +1,99 @@
+//
+//  RecordingViewController.swift
+//  Vois
+//
+//  Created by Sashankh Chengavalli Kumar on 15.03.20.
+//  Copyright © 2020 Vois. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordingViewController: UIViewController, AVAudioRecorderDelegate, UITableViewDelegate, UITableViewDataSource {
+    var recordingController: RecordingController!
+    var playbackController: PlaybackController!
+    var recordingCount = 0
+    var recordingDurationTimer: Timer!
+    var recordings: [URL]!
+
+    @IBOutlet weak var uiRecordButton: UIButton!
+    @IBOutlet weak var uiWaveForm: UIImageView!
+    @IBOutlet weak var uiTimeLabel: UILabel!
+    @IBOutlet weak var uiTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        recordingController = RecordingController()
+
+        recordings = recordingController.getRecordings()
+
+        if let storedRecordingCount: Int = UserDefaults.standard.object(forKey: "recordingCount") as? Int {
+            recordingCount = storedRecordingCount
+            recordingController = RecordingController(recordingCounter: recordingCount)
+        }
+    }
+    @IBAction func onRecordButtonTap(_ sender: UIButton) {
+        if recordingController.recordingInProgress {
+            stopRecording()
+            sender.setImage(UIImage(named: "Record Button.png"), for: .normal)
+        } else {
+            startRecording()
+            sender.setImage(UIImage(named: "Stop Button.png"), for: .normal)
+        }
+    }
+
+    func startRecordingDurationTimer() {
+        recordingDurationTimer = Timer.scheduledTimer(
+            timeInterval: 0.02,
+            target: self,
+            selector: #selector(self.updateRecordingDuration),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+
+    @objc func updateRecordingDuration() {
+        let secondsElapsed = recordingController.getCurrentRecordingDuration()
+        let seconds = secondsElapsed.truncatingRemainder(dividingBy: 60)
+        let minutes = Int(secondsElapsed / 60)
+
+        let secondsString = String(format: "%02d", Int(seconds))
+        let minutesString = String(format: "%02d", minutes)
+
+        uiTimeLabel.text = minutesString + ":" + secondsString
+    }
+
+    func startRecording() {
+        recordingController.startRecording(recorderDelegate: self)
+        startRecordingDurationTimer()
+    }
+
+    func stopRecording() {
+        recordingDurationTimer.invalidate()
+        recordingController.stopRecording()
+        recordings = recordingController.getRecordings()
+        uiTableView.reloadData()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recordings.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = recordings[indexPath.row].lastPathComponent
+        cell.textLabel?.textAlignment = .center
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        do {
+            playbackController = try PlaybackController(recordingFileName: recordings[indexPath.row])
+            playbackController.play()
+        } catch {
+            // TODO: Handle error
+        }
+
+    }
+}
