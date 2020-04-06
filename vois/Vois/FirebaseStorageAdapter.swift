96a9ec83055a60474051121d3a3f399c1284a0fa
Sashankh Chengavalli Kumar <cksashankh@gmail.com>
2020-04-06 13:22:48 +0800

    Add storage API

---
+++ vois/Vois/FirebaseStorageAdapter.swift
@@ -0,0 +1,101 @@
+//
+//  FirebaseStorageAdapter.swift
+//  Vois
+//
+//  Created by Sashankh Chengavalli Kumar on 06.04.20.
+//  Copyright © 2020 Vois. All rights reserved.
+//
+
+import Foundation
+import FirebaseStorage
+
+class FirebaseStorageAdapter {
+    var storage: Storage
+    var bucketName: String
+
+    init(bucketName: String) {
+        self.storage = Storage.storage()
+        self.bucketName = bucketName
+    }
+
+    convenience init() {
+        self.init(bucketName: "")
+    }
+
+    func getDocumentsDirectoryPath() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectoryPath = paths[0]
+        return documentDirectoryPath
+    }
+
+    func getReference(toPath: String) -> StorageReference {
+        let fullPath = bucketName + "/" + toPath
+        return storage.reference().child(fullPath)
+    }
+
+    func downloadFile(from: String, localFilePath: URL) {
+        let cloudFileReference = getReference(toPath: from)
+        let downloadTask = cloudFileReference.write(toFile: localFilePath)
+
+        downloadTask.observe(.success) { snapshot in
+            print("Success")
+            print(snapshot)
+        }
+
+        downloadTask.observe(.failure) { snapshot in
+            guard let errorCode = (snapshot.error as NSError?)?.code else {
+              return
+            }
+            guard let error = StorageErrorCode(rawValue: errorCode) else {
+              return
+            }
+
+            print("Failed due to error: \(error)")
+        }
+    }
+
+    func uploadFile(from: URL, to: String) {
+        let storageReference = getReference(toPath: to)
+
+        let uploadTask = storageReference.putFile(from: from, metadata: nil)
+
+        uploadTask.observe(.failure) { snapshot in
+            if let error = snapshot.error {
+                print("Failed due to error: \(error)")
+            }
+        }
+
+        uploadTask.observe(.success) { snapshot in
+            print("Succeded")
+            print(snapshot)
+        }
+    }
+
+    func deleteFile(at: String) {
+        let storageReference = getReference(toPath: at)
+
+        storageReference.delete() { error in
+            if let error = error {
+                print("Failed due to error: \(error)")
+            } else {
+                print("Deleted successfully")
+            }
+        }
+    }
+
+    func listFiles(at: String) -> [String] {
+        let storageReference = getReference(toPath: "recordings")
+
+        var items = [String]()
+        storageReference.listAll { (result, error) in
+            if let error = error {
+                print("Failed due to error: \(error)")
+            } else {
+                items = result.items.map { $0.name }
+                print("Items found are: \(items)")
+            }
+        }
+
+        return items
+    }
+}
